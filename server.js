7a56adf296d0113c7a07422181e652d3b460b31b
Ashok Kumar (Digital) <askmr61@wipro.com>
2016-08-09 23:04:03 +0530

    added port variable and huroku stuff

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+app.use(function(req, res, next){
+    // console.log("header ", req.headers['x-forwarded-proto']);
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        // console.log("url :", req.url);
+        // console.log('http://' + req.hostname + ":" + PORT);
+        res.redirect('http://' + req.hostname + ":" + req.url);
+    }
+});
+
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(4000, function(){
-    console.log('welcome to express app on port 4000');
+app.listen(PORT, function(){
+    console.log('welcome to express app on port ' + PORT);
 });
